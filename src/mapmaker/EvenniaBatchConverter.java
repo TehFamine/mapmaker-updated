563b746ef673d170ed2fc171490939c2ac8d0d26
Glen Swan <glenswan@gmail.com>
2025-03-09 12:21:46 -0400

    Adding initial Evennia Batch Converter class.

---
+++ src/mapmaker/EvenniaBatchConverter.java
@@ -0,0 +1,11 @@
+package mapmaker;
+import util.*;
+
+public class EvenniaBatchConverter
+        implements MapConverter {
+
+    public Text mapToText(AreaMap map, int startVnum, String fileName) {
+        return null;
+    }
+
+}
