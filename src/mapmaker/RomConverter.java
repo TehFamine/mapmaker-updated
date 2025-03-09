3d07f6799fd6ea65be4f8e92f0fbf6df3917c6a9
Glen Swan <glenswan@gmail.com>
2025-03-09 03:40:44 -0400

    Removing unused imports.

--- src/mapmaker/RomConverter.java
+++ src/mapmaker/RomConverter.java
@@ -1,9 +1,4 @@
 package mapmaker;
-
-import java.util.*;
-import javax.swing.*;
-import java.io.*;
-
 import util.*;
 
 /** used to convert a map into a ROM file
@@ -66,12 +61,11 @@ public class RomConverter
     out.append("{?? ??} MapMaker " + areaName + STRING_TERM);
     // get maximum vnum -> area vnums
     int maxVnum = 0;
-    Iterator it = roomVnums.values().iterator();
-    while (it.hasNext()) {
-      int vnum = ((Integer)it.next()).intValue();
-      if (vnum > maxVnum)
-	maxVnum = vnum;
-    }
+      for (Object o : roomVnums.values()) {
+          int vnum = (Integer) o;
+          if (vnum > maxVnum)
+              maxVnum = vnum;
+      }
     // range of vnums must be from __00 - __99
     int lastVnum = startVnum + 99;
     while (lastVnum < maxVnum)
@@ -81,32 +75,32 @@ public class RomConverter
 
     // the #ROOMS section
     out.append("#ROOMS");
-    for (int i = 0; i < rooms.length; i++) {
-      if (!(rooms[i] instanceof VirtualRoom)) {
-	// append non-link data
-	out.append("#" + roomVnums.get(rooms[i]));
-	out.append(getRoomName(rooms[i]));
-	out.append(getRoomDesc(rooms[i]));
-	out.append("0 " + 
-		   getRomRoomFlags(rooms[i]) + " " +
-		   RomConverterTool.getRoomSector(rooms[i]));
-	// append links
-	for (int dir = 0; dir < Dir.DIRNR; dir++)
-	  if (rooms[i].exitLinked(dir) && !rooms[i].exitBlocked(dir)) {
-	    Room exitRoom = 
-	      RomConverterTool.getExitRoom(rooms[i], rooms[i].getLink(dir));
-	    // exitRoom can be null -> no exit
-	    if (exitRoom != null) {
-	      out.append("D" + Dir.mercID(dir));
-	      out.append(STRING_TERM);
-	      out.append(STRING_TERM);
-	      out.append("0 0 " + roomVnums.get(exitRoom));
-	    }
-	  }
-	// finish room
-	out.append("S");
-      }
-    } // for (rooms)
+      for (Room room : rooms) {
+          if (!(room instanceof VirtualRoom)) {
+              // append non-link data
+              out.append("#" + roomVnums.get(room));
+              out.append(getRoomName(room));
+              out.append(getRoomDesc(room));
+              out.append("0 " +
+                      getRomRoomFlags(room) + " " +
+                      RomConverterTool.getRoomSector(room));
+              // append links
+              for (int dir = 0; dir < Dir.DIRNR; dir++)
+                  if (room.exitLinked(dir) && !room.exitBlocked(dir)) {
+                      Room exitRoom =
+                              RomConverterTool.getExitRoom(room, room.getLink(dir));
+                      // exitRoom can be null -> no exit
+                      if (exitRoom != null) {
+                          out.append("D" + Dir.mercID(dir));
+                          out.append(STRING_TERM);
+                          out.append(STRING_TERM);
+                          out.append("0 0 " + roomVnums.get(exitRoom));
+                      }
+                  }
+              // finish room
+              out.append("S");
+          }
+      } // for (rooms)
     out.append("#0");
     out.append("");
 
