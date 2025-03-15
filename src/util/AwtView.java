1c14e06597498cff25ab17e132c874392dc8d499
Glen Swan <glenswan@gmail.com>
2025-03-15 03:00:58 -0400

    Removing redundant public statements.

--- src/util/AwtView.java
+++ src/util/AwtView.java
@@ -6,14 +6,14 @@ public interface AwtView {
 
   /* paints to the given graphics context
    */
-  public void paint(Graphics g);
+  void paint(Graphics g);
 
   /* adds event listeneners to given component
    */
-  public void addListeners(Component speaker);
+  void addListeners(Component speaker);
 
   /* returns the view's preferred size
    */
-  public Dimension getSize();
+  Dimension getSize();
 
 } // AwtView 
